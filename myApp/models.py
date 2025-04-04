e50579cde81eb9cee77ed8128d502ab8a87ff75a
Ashkan <ashkan_ahrari@yahoo.com>
2025-04-04 12:23:58 +0300

    MODELS: amount,category,rating,recipe,ingredient added

--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
+from decimal import Decimal
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    title = models.CharField(max_length=264, unique=True)
+    
+    def __str__(self):
+        return self.title
+
+
+class Amount(models.Model):
+    amount = models.CharField(max_length=264, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=264, unique=True)
+    
+    def __str__(self):
+        return self.title
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=264)
+    instruction = models.CharField(max_length=264)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='images/')    
+    ingredients = models.ManyToManyField(Ingredient, related_name='recipes_ingredient')
+    amounts = models.ManyToManyField(Amount, related_name='recipes_amount')
+
+    def get_average_rating(self):
+        ratings = self.ratings.all()
+        if not ratings:
+            return Decimal('0.0')
+        else:
+            total = sum(rating.rate for rating in ratings)            
+        return Decimal(round(total / len(ratings), 1))
+    
+    # TODO: Or a better wayyyyy: 
+    # from django.db.models import Avg
+    # from decimal import Decimal
+    # def get_average_rating(self):
+    #     avg_rating = self.ratings.aggregate(avg=Avg("rate"))["avg"]
+    #     return Decimal(round(avg_rating, 1)) if avg_rating is not None else Decimal("0.0")
+    
+    def __str__(self):
+        return self.title    
+    
+
+class Rating(models.Model):
+    rate = models.DecimalField(max_digits=2, decimal_places=1, validators=[MaxValueValidator(5.0), MinValueValidator(1.0)])
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ratings')
+    date = models.DateTimeField(auto_now=True)
+    review = models.CharField(max_length=250, null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.user.username} rateed {self.rate} to {self.recipe.title}" 
