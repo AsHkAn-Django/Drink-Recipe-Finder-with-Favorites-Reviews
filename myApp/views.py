4dd62327780bcbbc6c59d1d62e6b9248a418bc22
Ashkan <ashkan_ahrari@yahoo.com>
2025-04-05 12:59:11 +0300

    add_to_favorite view rebuilt.

--- myApp/views.py
+++ myApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import generic
 import requests
 
-# from .models import Amount, Category, Rating, Recipe, Ingredient, Favorite
+from .models import RecipeIngredient, Category, Recipe, Ingredient, Favorite, Rating
 
 
 
@@ -41,48 +41,61 @@ class SearchView(generic.TemplateView):
 
 
 def add_to_favorite(request, pk):    
-    pass
-    # url = f"https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={pk}"
-    # try:
-    #     response = requests.get(url)
-    #     response.raise_for_status()
-    #     drink = response.json()['drinks'][0]
-    #     if not drink:  # Handle case where no drinks are found
-    #         drink = {"error": "No drinks found for this search!"}
-    # except:
-    #     drink = {"error": "An Error happened!!! Try Another Time!"}
+    url = f"https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={pk}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        drink = response.json()['drinks'][0]
+        if not drink:  # Handle case where no drinks are found
+            drink = {"error": "No drinks found for this search!"}
+    except:
+        drink = {"error": "An Error happened!!! Try Another Time!"}
         
-    # print(drink)
-    # if drink:
-    #     if Recipe.objects.filter(recipe_id=drink['idDrink']).exists():
-    #         print("we've already had this item in the database.")
-    #     else:
-    #         print("we don't have this drink in the database and we should add it.")
-    #         ingredients_list = []
-    #         amounts_list = []
-    #         for number in range(1, 16):
-    #             if drink[f'strMeasure{number}'] is None or drink[f'strIngredient{number}'] is None:
-    #                 break
-    #             else:                    
-    #                 ingredients_list.append(drink[f'strIngredient{number}'])
-    #                 amounts_list.append(drink[f'strMeasure{number}'])
+    print(drink)
+    if drink:
+        if Recipe.objects.filter(recipe_id=drink['idDrink']).exists():
+            print("we've already had this item in the database.")
+        else:
+            print("we don't have this drink in the database and we should add it.")
+            # First we get or create the category            
+            drink_category, _ =  Category.objects.get_or_create(name=drink['strCategory'])
 
-    #         drink_category, _ =  Category.objects.get_or_create(title=drink['strCategory'])
-    #         recipe = Recipe.objects.create(recipe_id=drink['idDrink'],
-    #                                 title=drink['strDrink'],
-    #                                 instruction=drink['strInstructions'],
-    #                                 category=drink_category,
-    #                                 picture_url=drink['strDrinkThumb'],)   
+            # Then add the recipe
+            recipe = Recipe.objects.create(
+                        recipe_id=drink['idDrink'],
+                        title=drink['strDrink'],
+                        instructions=drink['strInstructions'],
+                        category=drink_category,
+                        picture_url=drink['strDrinkThumb'],)   
             
-    #         for index in range(len(amounts_list)):
-    #             ingredient, _ = Ingredient.objects.get_or_create(title=ingredients_list[index])
-    #             amount, _ = Amount.objects.get_or_create(title=amounts_list[index])
-    #             recipe.ingredients.add(ingredient)
-    #             recipe.amounts.add(amount)
-                
-    #         Favorite.objects.create(user=request.user, recipe=recipe)
-   
-    # return redirect('my_favorites')
+            # then the ingredients and amounts through RecipeIngredient Model
+            # first we get the ingredients, amounts and orders
+            ingredients_list = []
+            amounts_list = []
+            order_list = []
+            for number in range(1, 16):
+                if drink[f'strMeasure{number}'] is None or drink[f'strIngredient{number}'] is None:
+                    break
+                else:
+                    ingredients_list.append(drink[f'strIngredient{number}'])
+                    amounts_list.append(drink[f'strMeasure{number}'])
+                    order_list.append(number)
+            
+            # Then we add them in the database through RecipeIngredient Model
+            for index in range(len(amounts_list)):
+                # get or create the ingredient
+                ingredient, _ = Ingredient.objects.get_or_create(name=ingredients_list[index])
+                # record the recipeIngredient
+                RecipeIngredient.objects.create(
+                    recipe=recipe,
+                    ingredient=ingredient,
+                    amount=amounts_list[index],
+                    order=order_list[index]
+                )                    
+            
+            # add it to user's favorite    
+            Favorite.objects.create(user=request.user, recipe=recipe)
+    return redirect('search')
 
 
 def my_favorites(request):
